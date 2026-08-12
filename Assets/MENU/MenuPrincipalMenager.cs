13eaeff3282ebe3aa9072ebbfe9d09715cc8922c
Vinicius Oliveira Cunha Claro <vinicius.claro@eaportal.org>
2026-08-12 09:42:10 -0300

    MENU+PLAY

--- Assets/MENU/MenuPrincipalMenager.cs
+++ Assets/MENU/MenuPrincipalMenager.cs
@@ -12,7 +12,8 @@ public class MenuPrincipalManager : MonoBehaviour, IPointerEnterHandler, IPointe
 
     public void Jogar()
     {
-        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    Debug.Log("CLIQUEI NO PLAY");
+    SceneManager.LoadScene(nomeDoLevelDeJogo);
     }
 
     public void AbrirOpcoes()
