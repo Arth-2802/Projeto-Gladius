04101f384f6664b94205770fcf7160cf4bb9e519
Vinicius Oliveira Cunha Claro <vinicius.claro@eaportal.org>
2026-08-19 19:23:10 -0300

    engrenagem no menu

--- Assets/MENU/MenuPrincipalMenager.cs
+++ Assets/MENU/MenuPrincipalMenager.cs
@@ -12,29 +12,39 @@ public class MenuPrincipalManager : MonoBehaviour, IPointerEnterHandler, IPointe
 
     public void Jogar()
     {
-    SceneManager.LoadScene(nomeDoLevelDeJogo);
-    Debug.Log("CLIQUEI NO PLAY");
+        Debug.Log("CLIQUEI NO PLAY");
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
     }
 
     public void AbrirOpcoes()
     {
+        Debug.Log("CLIQUEI EM OPÇÕES");
         painelMenuInicial.SetActive(false);
         painelOpcoes.SetActive(true);
+        Debug.Log("Painel opções ativo: " + painelOpcoes.activeSelf);
     }
 
     public void FecharOpcoes()
     {
+        Debug.Log("CLIQUEI EM FECHAR OPÇÕES");
         painelOpcoes.SetActive(false);
         painelMenuInicial.SetActive(true);
     }
 
     public void SairJogo()
     {
-        Debug.Log("Sair do jogo");
+        Debug.Log("CLIQUEI EM SAIR");
         Application.Quit();
     }
 
     // ✅ Métodos obrigatórios das interfaces
-    public void OnPointerEnter(PointerEventData eventData) { }
-    public void OnPointerExit(PointerEventData eventData) { }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log($"[Foco] Mouse entrou em: {gameObject.name}");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log($"[Foco] Mouse saiu de: {gameObject.name}");
+    }
 }
